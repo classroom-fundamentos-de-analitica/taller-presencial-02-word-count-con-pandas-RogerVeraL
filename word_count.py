6ac595195cf5f12c5d006ab538bc2315afcf1abb
RogerVeraL <105682754+RogerVeraL@users.noreply.github.com>
2024-02-19 22:06:37 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -14,7 +14,7 @@ def load_input(input_directory):
     #   
     filenames = glob.glob(input_directory + "/*.*")
     dataframes = [
-        pd.read_csv(filename, sep = ";" ) for filename in filenames
+        pd.read_csv(filename, sep = ";" , names=['text']) for filename in filenames
     ]
     dataframe = pd.concat(dataframes).reset_index(drop=True)
     return dataframe
